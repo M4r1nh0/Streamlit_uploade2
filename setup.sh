716d722bf00add126dac17b54c1c9cdd34b24a5c
Daniel marinho <37985059+M4r1nh0@users.noreply.github.com>
2020-11-19 01:51:55 -0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"danielmarinho367433818@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
